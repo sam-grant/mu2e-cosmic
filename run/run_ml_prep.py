e926f982e9ce55cca174f55a3ce405083bebe856
sam-grant <samuel.grant94@gmail.com>
2025-11-06 20:25:57 +0000

    Improved modulatiry

--- run/run_ml_prep.py
+++ run/run_ml_prep.py
@@ -13,47 +13,49 @@ from ml_process import MLProcessor
 from draw import Draw
 from io_manager import Write
 
-def run_ml_prep():
-    
-    # Inputs...
-    # signal_file = "../input/files/nts.mu2e.CosmicCRYSignalAllOnSpillTriggered.MDC2020aw_perfect_v1_3_v06_06_00.001202_00042784.root"
-    # background_file = "../input/files/nts.mu2e.CeEndpointOnSpillTriggered.MDC2020aw_perfect_v1_3_v06_06_00.001210_00000580.root"
-    signal_defname = "nts.mu2e.CeEndpointOnSpillTriggered.MDC2020aw_perfect_v1_3_v06_06_00.root"
-    background_defname = "nts.mu2e.CosmicCRYSignalAllOnSpillTriggered.MDC2020aw_perfect_v1_3_v06_06_00.root"
+def run(defname, tag, feature_set, run="a"):
 
-    # nts.mu2e.CosmicCRYSignalAllMix2BBTriggered.MDC2020aw_best_v1_3_v06_06_00.root
-    # nts.mu2e.CeEndpointMix2BBTriggered.MDC2020aw_best_v1_3_v06_06_00.root
+    # Output directory
+    out_path = Path(f"../output/ml/veto/data/{run}/{tag}")
+    out_path.mkdir(parents=True, exist_ok=True)
 
-    # Output directories
-    signal_out_path = Path("../output/ml/data/b/sig")
-    background_out_path = Path("../output/ml/data/b/bkg")
-    # Create directories (including parents) if they don't exist
-    signal_out_path.mkdir(parents=True, exist_ok=True)
-    background_out_path.mkdir(parents=True, exist_ok=True)
-    
     # Processing 
-    results = {} 
-    
-    # Signal
     process = MLProcessor(
-        # file_name=signal_file,
-        defname=signal_defname
+        defname=defname,
+        # file_name="/exp/mu2e/data/users/sgrant/mu2e-cosmic/files/nts.mu2e.CosmicCRYSignalAllMix2BBTriggered.MDC2020aw_best_v1_3_v06_05_00.001202_00056492.root",
+        feature_set=feature_set,
+        use_remote=True
     ) 
-    results["signal"] = process.execute()
-    
-    # Background
-    process = MLProcessor(
-        # file_name=background_file, 
-        defname=background_defname
-    ) 
-    results["background"] = process.execute()
-    
-    # save results
-    Write(out_path=signal_out_path).write_all(results["signal"])
-    Write(out_path=background_out_path).write_all(results["background"])
+    results = process.execute()
 
+    # Save results
+    Write(out_path=out_path).write_all(results)
+    
 def main(): 
-    run_ml_prep()
+
+    configs = [
+        # {
+        #     defname: "nts.mu2e.CeEndpointOnSpillTriggered.MDC2020aw_perfect_v1_3_v06_06_00.root",
+        #     tag: "sig_onspill-LH_aw",
+        # },
+        # {
+        #     "defname": "nts.mu2e.CeEndpointMix2BBTriggered.MDC2020aw_best_v1_3_v06_06_00.root",
+        #     "tag": "CRY_onspill-LH_aw",
+        # },
+        # {
+        #     defname: "nts.mu2e.CeEndpointMix2BBTriggered.MDC2020aw_best_v1_3_v06_06_00.root",
+        #     tag: "sig_mix2BB_onspill-LH_aw",
+        # },
+        {
+            "defname": "nts.mu2e.CosmicCRYSignalAllMix2BBTriggered.MDC2020aw_best_v1_3_v06_06_00.root",
+            "tag": "CRY_mix2BB_onspill-LH_aw",
+            "feature_set": "crv"
+        },
+    ]
+
+    
+    for config in configs:
+        run(config["defname"], config["tag"], config["feature_set"]) 
 
 if __name__ == "__main__":
     main()
