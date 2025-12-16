b4d0a3d6ae09951f97c7d23dcaf8b1e86b404346
Samuel Paul Grant <Samuel Grant>
2025-12-16 11:16:38 -0600

    Added process validation

--- src/ml/process_validation.py
+++ src/ml/process_validation.py
@@ -22,9 +22,12 @@ class ProcessValidation():
         # A bit hacky but...
         # this is defined from the notebook/ml location
         self.img_out_path = Path(f"../../../output/images/ml/{self.run}/process/") 
-        
+        self.cry_data, self.ce_mix_data = LoadML(run=self.run).get_full_results()
+
     def draw_hists(self):
-        cry_data, ce_mix_data = LoadML(run=self.run).get_full_results()
         draw = Draw()
-        draw.plot_summary(cry_data["hists"], out_path = self.img_out_path / "h1o_3x3_cuts_CRY.png")
-        draw.plot_summary(ce_mix_data["hists"], out_path = self.img_out_path / "h1o_3x3_cuts_CE_mix.png")
+        draw.plot_summary(self.cry_data["hists"], out_path = self.img_out_path / "h1o_3x3_cuts_CRY.png")
+        draw.plot_summary(self.ce_mix_data["hists"], out_path = self.img_out_path / "h1o_3x3_cuts_CE_mix.png")
+        
+    def get_cut_flows(self):
+        return self.cry_data["cut_flow"], self.ce_mix_data["cut_flow"]
