ff3ccda3c79cc0970e334c74133c19d1d18158e7
Simon Corrodi <scorrodi@anl.gov>
2026-01-16 18:03:53 -0600

    Livetime script works

--- scripts/batch_livetime.sh
+++ scripts/batch_livetime.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# muse setup SimJob for Offline
+# muse setup ops for mdh
 
-DEFNAME=$1
+DEFNAME=$1 # needs to be the parent dataset...
+
 parallel --progress -j 48 \
-    'file={}; xrpath=$(mdh print-url "${file}" -l tape -s root); mu2e -c Offline/Print/fcl/printCosmicLivetime.fcl -s "${xrpath}" | grep "Livetime:" | awk -F: "{print \$NF}"' \
-    < <(samweb list-definition-files $DEFNAME) > ${DEFNAME}.livetime
+     'file={}; xrpath=$(mdh print-url "${file}" -l tape -s root); mu2e -c Offline/Print/fcl/printCosmicLivetime.fcl -s "${xrpath}" | grep "Livetime:" | awk -F: "{print \$NF}"' \
+     < <(samweb list-definition-files $DEFNAME) > ${DEFNAME}.livetime
 
+awk '{sum += $1} END {print "Total livetime:", sum, "sec"}' "${DATASET}.livetime"
